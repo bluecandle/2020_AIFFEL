1fe31355c4175317f382c224771e6fcf40991d9f
bluecandle <jinyeongkim37@gmail.com>
2020-07-31 15:08:20 +0900

    Day7/LMS F-5, codingMaster seesion2 pb2,pb3

--- coding_master/Session2/problem3.py
+++ coding_master/Session2/problem3.py
@@ -13,10 +13,17 @@
 # [3, 0, 6, 1, 5]	3
 # 입출력 예 설명
 # 이 과학자가 발표한 논문의 수는 5편이고, 그중 3편의 논문은 3회 이상 인용되었습니다. 그리고 나머지 2편의 논문은 3회 이하 인용되었기 때문에 이 과학자의 H-Index는 3입니다.
-citations = [3, 0, 6, 1, 5]
+
+# citation 숫자 배열 안에서 가장 많이 인용된 횟수부터 시작하여, 순차적으로 1씩 감소시켜보며 진행한다.
+# h가 0이 될 때까지 진행하며,h 가 0이 되는 경우 답은 0이 된다. ( 모든 논문이 한 번도 인용된 적이 없음)
+# h 번 이상 인용된 논문들을 골라내고, 골라진 논문들의 수가 h 개 이상이라면, 우선 1단계 통과
+# 단, 골라진 논문들을 내림차순으로 정렬했을 때 h 번째 이후에 있는 대상들을 확인해봐야한다.
+# 해당 대상들이 h 번을 초과하는 인용 횟수를 가졌다면, 조건에 성립하지 않으므로, 다음 h 항목으로 넘어가야 한다.
+
 def solution(citations):
     answer=0   
     for h in range(max(citations),0,-1):
+        # print(h)
         _l = sorted(list(filter(lambda x: x>=h, citations)),reverse=True)
         # print(_l)
         if(len(_l)>=h):
@@ -31,5 +38,6 @@ def solution(citations):
                 break
 
     return answer
-print(solution(citations))
-print(solution([4,4,2,7]))
+# print(solution([3, 0, 6, 1, 5]))
+# print(solution([4,4,2,7]))
+# print(solution([0,0,0,0]))
